dfb9148baac2d234fe5c47904fee0fb1f6a3a693
Dilip Kr. Shukla <contact@fusionstrings.com>
2018-08-04 14:44:54 +0200

    fix local development

--- functions/app/dev-server.js
+++ functions/app/dev-server.js
@@ -1,16 +1,15 @@
 import sirv from 'sirv';
 import polka from 'polka';
-import sapper from 'sapper';
 import compression from 'compression';
-import {handler} from './middleware.js';
 
-polka() // You can also use Express
-	.use(
-		compression({ threshold: 0 }),
-		sirv('assets'),
-		handler
-	)
-	.listen(process.env.PORT)
-	.catch(err => {
-		console.log('error', err);
-	})
+const server = (middleware) => {
+	console.log('server starting');
+	polka() // You can also use Express
+		.use(compression({ threshold: 0 }), sirv('assets'), middleware)
+		.listen(process.env.PORT || 3000)
+		.catch(err => {
+			console.log('error', err);
+		});
+};
+
+export default server;
--- functions/app/server.js
+++ functions/app/server.js
@@ -1,4 +1,4 @@
-import sapper from 'sapper';
-import { manifest } from './manifest/server.js';
+import { middleware  } from './middleware.js';
+import server from './dev-server.js';
 
-export const handler = sapper({manifest});
+export const handler = process.env.DEV === 'local' ? server(middleware) : middleware;
